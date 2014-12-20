d33f73af6cc110b4cba28c023d8c2fc5efcc969d
breakpoint-brian <brian@breakpointdev.com>
2014-12-20 09:41:19 -0600

    ..

--- mysql/mainpage.php
+++ mysql/mainpage.php
@@ -4,6 +4,7 @@
 	$query ="SELECT `entry` FROM `users` WHERE id='".$_SESSION['id']."' LIMIT 1";
 	$result = mysqli_query($link, $query);
 	$row = mysqli_fetch_array($result);
+	print_r($_SESSION);
 	print_r($row);
 	$entry = $row['entry'];
 ?>
